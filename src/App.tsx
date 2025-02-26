ca76efe1188735b75855ceb54ce29d1b76d58753
Isaac Trost <isaacmichaeltrost@gmail.com>
2025-02-26 14:13:15 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import Carousel from "./components/Carousel";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+function App() {
+  return (
+    <>
+      <Carousel></Carousel>
+    </>
+  );
+}
+
+export default App;
